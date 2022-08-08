96c87b80e178cb06a4006d2743e667cc4d50388f
JANARTHANAN B <95681443+JANARTHANAN13102002@users.noreply.github.com>
2022-08-08 15:48:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//change navbar style on scroll
+
+window.addEventListener('scroll',() =>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll',window.scrollY >0)
+})
+
+
+// show/hide the FAQ 
+
+const faqs =document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        }else{
+            icon.className ="uil uil-plus"
+        }
+    })
+})
